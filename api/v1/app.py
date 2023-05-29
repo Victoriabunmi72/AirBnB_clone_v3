5916b1a5dedcfabf1ea7c067b13c1e8fec36005e
sandymaramma <edesandraoge@gmail.com>
2023-05-29 07:27:24 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 """a folder api at the root of the project with an empty file __init__.py"""
 
-from flask import Flask
-from api.v1.views import app_views
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
+from api.v1.views import app_views
+from os import getenv
 
-app = Flask(__name__)
+app = Flask('v1')
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+CORS(app, resources=r"/api/v1/*", origins="*")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 not found errors and return json object"""
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def close_storage(*args, **kwargs):
+    """Clost app storage (FileStorage or DBStorage)"""
     storage.close()
 
 
-if __name__ == '__main__':
-    import os
-
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-
-    app.run(host=host, port=port, threaded=True)
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=int(port))
 
